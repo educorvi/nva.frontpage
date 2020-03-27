b4465714095fa06e75eaf86349a8f750cd415695
pascalpaul <pascalpaul@educorvi.de>
2020-03-27 10:47:29 +0100

    "Add view: rightimageview"

--- src/nva/frontpage/content/rightimage.py
+++ src/nva/frontpage/content/rightimage.py
@@ -8,6 +8,7 @@ from plone.supermodel import model
 # from z3c.form.browser.radio import RadioFieldWidget
 # from zope import schema
 from zope.interface import implementer
+from plone.namedfile.field import NamedBlobImage
 
 
 # from nva.frontpage import _
@@ -16,6 +17,7 @@ from zope.interface import implementer
 class IRightimage(model.Schema):
     """ Marker interface and Dexterity Python Schema for Rightimage
     """
+    bild = NamedBlobImage(title="Vorschaubild")
     # If you want, you can load a xml model created TTW here
     # and customize it in Python:
 
