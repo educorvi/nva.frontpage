c9a54a87a005ab183050823056add3e4f47ee354
seppowalther <seppo.walther@educorvi.de>
2020-03-30 10:44:21 +0200

    Working on leftimageview and content type textbox

--- src/nva/frontpage/content/textbox.py
+++ src/nva/frontpage/content/textbox.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-# from plone.app.textfield import RichText
+from plone.app.textfield import RichText
 # from plone.autoform import directives
 from plone.dexterity.content import Item
 # from plone.namedfile import field as namedfile
 from plone.supermodel import model
 # from plone.supermodel.directives import fieldset
 # from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
+from zope import schema
 from zope.interface import implementer
 
 
@@ -16,6 +16,8 @@ from zope.interface import implementer
 class ITextbox(model.Schema):
     """ Marker interface and Dexterity Python Schema for Textbox
     """
+
+    textbox = RichText(title="Textbox")
 
     fontcolor = schema.TextLine(title="Schriftfarbe")
     bordercolor = schema.TextLine(title="BorderColor")
